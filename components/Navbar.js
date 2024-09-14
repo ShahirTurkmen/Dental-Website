8ea6088903e1b8f9b02c1afbed52ccf296cf1696
ShahirTurkmen <shahirturkmen@gmail.com>
2024-09-14 19:52:28 +0430

    Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+
+
+`
+function Navbar() {
+  return (
+      <div>
+          <div></div>
+    </div>
+  )
+}
+
+export default Navbar

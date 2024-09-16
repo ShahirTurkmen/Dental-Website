e2429bbf8dc3d8dfad13edc2e079076bb3357099
ShahirTurkmen <shahirturkmen@gmail.com>
2024-09-16 18:55:06 +0430

    Hero

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
 import React from 'react'
 
@@ -8,6 +9,7 @@ function Home() {
     <>
       <Header />
       <Navbar />
+      <Hero/>
     </>
   );
 }

ca7e96d2f400509c4b2d3d24073e072c372b2b1f
ShahirTurkmen <shahirturkmen@gmail.com>
2024-09-20 09:20:37 +0430

    page

--- app/page.js
+++ app/page.js
@@ -1,18 +1,19 @@
 // 'use client'
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import Navbar from '@/components/Navbar';
-import React from 'react'
-
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import React from "react";
 
 function Home() {
   return (
     <>
       <Header />
       <Navbar />
-      <Hero/>
+      <Hero />
+      <Footer />
     </>
   );
 }
 
-export default Home
+export default Home;
